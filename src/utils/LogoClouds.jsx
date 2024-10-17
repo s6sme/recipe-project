955dc6f1645442634e958f42ff925739051fa6d2
s6sme <114183268+s6sme@users.noreply.github.com>
2024-10-17 20:41:21 +0300

    Update LogoClouds.jsx

--- src/utils/LogoClouds.jsx
+++ src/utils/LogoClouds.jsx
@@ -8,7 +8,8 @@ export default function LogoClouds() {
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
+{/*             src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg" */}
+            src="https://vuetifyjs.b-cdn.net/docs/images/one/logos/vbin-logo.png"
             alt="Transistor"
             width={158}
             height={48}
