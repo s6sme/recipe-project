15b9c25b8fc30e2d3c763c65915be2456393f848
s6sme <114183268+s6sme@users.noreply.github.com>
2024-10-17 20:57:25 +0300

    Update LogoClouds.jsx

--- src/utils/LogoClouds.jsx
+++ src/utils/LogoClouds.jsx
@@ -8,35 +8,35 @@ export default function LogoClouds() {
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://img.freepik.com/premium-vector/go-web-visit-our-website-world-globe-with-arrow-cursor-symbol-vector-icon_1163725-2402.jpg"
+            src="https://www.designmantic.com/logo-images/172837.png?company=Company%20Name&keyword=courier&slogan=&verify=1"
             alt="Transistor"
             width={158}
             height={48}
           />
           <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://img.freepik.com/premium-vector/go-web-visit-our-website-world-globe-with-arrow-cursor-symbol-vector-icon_1163725-2402.jpg"
+            src="https://t3.ftcdn.net/jpg/04/73/02/64/360_F_473026422_k3XjtqTh0Br3Iw8IfhlB9c72n9dqi9n5.jpg"
             alt="Reform"
             width={158}
             height={48}
           />
           <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://img.freepik.com/premium-vector/go-web-visit-our-website-world-globe-with-arrow-cursor-symbol-vector-icon_1163725-2402.jpg"
+            src="https://scontent.fiev13-1.fna.fbcdn.net/v/t39.30808-6/327185707_590840502419946_3307563807964723860_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=H3yoci8iZ6kQ7kNvgHkPwt9&_nc_zt=23&_nc_ht=scontent.fiev13-1.fna&_nc_gid=AP9ZEiHrNwBd8kbBr7NtzIp&oh=00_AYBxqiyMJSw-pjNBUCM3ZroA1D89OPYTLGgMZEMBkMjA6A&oe=67172772"
             alt="Tuple"
             width={158}
             height={48}
           />
           <img
             className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-            src="https://img.freepik.com/premium-vector/go-web-visit-our-website-world-globe-with-arrow-cursor-symbol-vector-icon_1163725-2402.jpg"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Current_Logo_Glovo.jpg/1200px-Current_Logo_Glovo.jpg"
             alt="SavvyCal"
             width={158}
             height={48}
           />
           <img
             className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-            src="https://img.freepik.com/premium-vector/go-web-visit-our-website-world-globe-with-arrow-cursor-symbol-vector-icon_1163725-2402.jpg"
+            src="https://i.pinimg.com/736x/ba/98/3f/ba983ff381da1006458b50ef9947a115.jpg"
             alt="Statamic"
             width={158}
             height={48}
